51538831908a6f9f4be02da7705a884906638bfe
Arthur Barrett <abarrett@fas.harvard.edu>
2012-10-31 18:05:53 -0400

    Added methods and tests to the TagCollection model.

--- app/Model/TagCollection.php
+++ app/Model/TagCollection.php
@@ -3,6 +3,8 @@
 // Use of this source code is governed by the LICENSE file found in the root of this project.
 ?>
 <?php
+App::uses('String', 'Utility');
+
 /**
  * TagCollection Model
  *
@@ -13,15 +15,86 @@ class TagCollection extends AppModel {
 /**
  * Constant for the algorithm used to calculate the tag collection hash.
  */
-	const HASH_ALGO = 'sha1';
+	const HASH_ALGORITHM = 'sha1';
+	
+/**
+ * Constant for tag list separator.
+ */
+ 	const TAG_SEPARATOR = ',';
 
+/**
+ * Parses a comma-separated string of tags.
+ *
+ * @param string $tag_str a comma-separated list of tags
+ * @return array of tags
+ */
+ 	public function parseTags($tag_str = '') {
+ 		$tags = array();
+ 		$items = explode(self::TAG_SEPARATOR, $tag_str);
+ 		
+ 		foreach($items as $item) {
+ 			$tag = preg_replace( "/(^\s+)|(\s+$)/us", "", $item);
+ 			$is_valid_trimmed_tag = ($tag !== null && $tag !== '');
+ 			if($is_valid_trimmed_tag) {
+ 				$tags[] = $tag;
+ 			}
+ 		}
+ 
+ 		return $tags;
+ 	}
+
+/**
+ * Formats a list of tags as a string.
+ *
+ * @param array $tags a list of tags
+ * @return string of tags
+ */
+ 	public function stringify($tags = array()) {
+ 		return implode(self::TAG_SEPARATOR, $tags);
+ 	}
+ 	
+ /**
+  * Formats a list of tags as a string in canonical order.
+  *
+  * Used primarily in conjunction with the hash function.
+  *
+  * @param array $tags a list of tags
+  * @returns string of tags in canonical order
+  */
+  	public function stringifyCanonical($tags = array()) {
+  		$sorted_tags = $this->sortTags($tags);
+  		return $this->stringify($sorted_tags);
+  	}
+ 
+ /**
+  * Sort a list of tags.
+  *
+  * @param array $tags a list of tags
+  * @return array of sorted tags
+  */
+  	public function sortTags($tags = array()) {
+  		// will sort according to the current locale (see setlocale())
+  		usort($tags, 'strcoll'); // strcoll equivalent to strcmp() if current locale is C or POSIX
+  		return $tags;
+  	}
+ 
+	
 /**
  * Calculates the hash value of a list of tags.
  *
- * @param string $input a comma-separated list of tags
- * @return hash value
+ * @param string $tags a list of tags
+ * @return false if no tags to hash, otherwise the value of the hash() function
  */
-	public function hashOf($input = '') {
-		return hash(self::HASH_ALGO, $input, FALSE);
+	public function hashOf($tags = array()) {
+		if(is_string($tags)) {
+			$tags = $this->parseTags($tags);
+		}
+		$tag_str = $this->stringifyCanonical($tags);
+		
+		if($tag_str === '') {
+			return false;
+		}
+
+		return hash(self::HASH_ALGORITHM, $tag_str, FALSE);
 	}
 }
--- app/Test/Case/Model/TagCollectionTest.php
+++ app/Test/Case/Model/TagCollectionTest.php
@@ -39,14 +39,101 @@ class TagCollectionTestCase extends CakeTestCase {
 	}
 
 /**
+ * testParseTags
+ *
+ * @return void
+ */
+ 	public function testParseTags() {
+ 		$tests = array(
+ 			array('data' => '', 'expected' => array()),
+ 			array('data' => 'a,', 'expected' => array('a')),
+ 			array('data' => ',a', 'expected' => array('a')),
+  			array('data' => 'a,b,c', 'expected' => array('a', 'b', 'c')),
+ 			array('data' => 'a, ,c', 'expected' => array('a', 'c')),
+ 			array('data' => 'a b c', 'expected' => array('a b c'))
+ 		);
+ 		
+ 		foreach($tests as $test) {
+ 			$actual = $this->TagCollection->parseTags($test['data']);
+ 			$expected = $test['expected'];
+ 			$this->assertEquals($expected, $actual, 'parsing tags');
+ 		}
+ 	}
+
+/**
+ * testStringify
+ *
+ * @return void
+ */
+ 	public function testStringify() {
+ 		$tests = array(
+ 			array('data' => array(), 'expected' => ''),
+ 			array('data' => array(''), 'expected' => ''),
+ 			array('data' => array('a'), 'expected' => 'a'),
+ 			array('data' => array('a','b'), 'expected' => 'a,b'),
+ 			array('data' => array('c','b','a'), 'expected' => 'c,b,a'),
+ 			array('data' => array('a','b','c'), 'expected' => 'a,b,c')
+ 		);
+ 		
+ 		foreach($tests as $test) {
+ 			$actual = $this->TagCollection->stringify($test['data']);
+ 			$expected = $test['expected'];
+ 			$this->assertEquals($expected, $actual, 'stringify tags');
+ 		}
+ 	}
+
+/**
+ * testStringifyCanonical
+ *
+ * @return void
+ */
+ 	public function testStringifyCanonical() {
+ 		$tests = array(
+ 			array('data' => array('c','b','a'), 'expected' => 'a,b,c'),
+ 			array('data' => array('a','b','c'), 'expected' => 'a,b,c')
+ 		);
+ 		
+ 		foreach($tests as $test) {
+ 			$actual = $this->TagCollection->stringifyCanonical($test['data']);
+ 			$expected = $test['expected'];
+ 			$this->assertEquals($expected, $actual, 'stringify tags in canonical order');
+ 		}
+ 	}
+ 	
+/**
+ * testSortTags
+ *
+ * @return void
+ */
+ 	public function testSortTags() {
+ 		$tests = array(
+ 			array('data' => array(), 'expected' => array()),
+ 			array('data' => array('a'), 'expected' => array('a')),
+ 			array('data' => array('c','b','a'), 'expected' => array('a','b','c')),
+ 			array('data' => array('uno','dos','tres'), 'expected' => array('dos', 'tres','uno'))
+ 		);
+ 		
+ 		foreach($tests as $test) {
+ 			$actual = $this->TagCollection->sortTags($test['data']);
+ 			$expected = $test['expected'];
+ 			$this->assertEquals($expected, $actual, 'sorted tags by locale collation');
+ 		}
+ 	}
+ 
+/**
  * testHashOf
  * 
  * @return void
  */
 	public function testHashOf() {
-		$tag_str = 'one,two,three';
-		$expected_hash = hash(TagCollection::HASH_ALGO, $tag_str, FALSE);
-		$actual_hash = $this->TagCollection->hashOf($tag_str);
-		$this->assertEquals($expected_hash, $actual_hash, 'hash values match for list of tags in the collection');
+		$tags = array('uno', 'dos', 'tres');
+		$tag_str = $this->TagCollection->stringifyCanonical($tags);
+
+		$expected = hash(TagCollection::HASH_ALGORITHM, $tag_str, FALSE);
+		$actual = $this->TagCollection->hashOf($tags);
+
+		$this->assertEquals($expected, $actual, 'hash values match for list of tags in the collection');
+		$this->assertFalse($this->TagCollection->hashOf(array()), 'no tags to hash');
+		$this->assertFalse($this->TagCollection->hashOf(''), 'empty tag string to hash');
 	}
 }
