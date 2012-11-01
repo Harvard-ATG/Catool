f7ac3261299f0a4646361c2050bbd19219311a8b
Arthur Barrett <abarrett@fas.harvard.edu>
2012-11-01 16:50:32 -0400

    Added hook functions that will be overridden for taggable behavior.

--- app/Model/Behavior/TaggableBehavior.php
+++ app/Model/Behavior/TaggableBehavior.php
@@ -25,4 +25,68 @@ App::uses('AppModel', 'Model');
  * @package       app.Model.Behavior
  */
 class TaggableBehavior extends ModelBehavior {
+	
+/**
+ * Settings per model.
+ *
+ * @var array
+ */
+	public $settings = array();
+
+/**
+ * Setup this behavior with the specified configuration settings.
+ * 
+ * @param Model $Model Model using this behavior
+ * @param array $settings Configuration settings for $model
+ * @return void
+ */
+	public function setup(Model $Model, $settings = array()) {
+		$default_settings = array('foreignKey' => 'tag_collection_id');
+		$this->settings[$Model->alias] = array_merge($default_settings, $settings);
+	}
+	
+/**
+ * beforeValidate callback
+ *
+ * @param Model $Model Model using this behavior
+ * @return boolean False or null will abort the operation. Any other result will continue.
+ */
+	public function beforeValidate(Model $Model) {
+		return true;
+	}
+
+/**
+ * beforeSave callback
+ *
+ * @param Model $Model Model using this behavior
+ * @param array $options
+ * @return boolean
+ */
+	public function beforeSave(Model $Model, $options = array()) {
+		return true;
+	}
+
+/**
+ * afterSave callback
+ *
+ * @param Model $Model Model using this behavior
+ * @param boolean $created True if this save created a new record
+ * @return void
+ */
+	public function afterSave(Model $Model, $created) {
+		return true;
+	}
+
+/**
+ * afterFind callback
+ *
+ * @param Model $Model Model using this behavior
+ * @param mixed $result The results of the find operation
+ * @param boolean $primary Whether this model is being queried directly (vs. being queried as an association)
+ * @return void
+ */
+	public function afterFind(Model $Model, $result, $primary) {
+		return $result;
+	}
+
 }
